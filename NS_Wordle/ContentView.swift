74b65054316d4258b6df10a434825469e72396a0
Nikhil Shende <nikhil.shende@gomotive.com>
2022-07-08 13:39:25 -0400

    Initial Commit

---
+++ NS_Wordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NS_Wordle
+//
+//  Created by Nikhil Shende on 2022-07-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NS_Wordle/NS_WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NS_WordleApp.swift
+//  NS_Wordle
+//
+//  Created by Nikhil Shende on 2022-07-08.
+//
+
+import SwiftUI
+
+@main
+struct NS_WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
